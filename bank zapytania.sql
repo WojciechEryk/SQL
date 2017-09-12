47429653adf1dace62b045e07667625af521af41
WojciechPC\Wojciech <w.g.michalik@gmail.com>
2017-09-12 17:03:47 +0200

    bank

---
+++ bank zapytania.sql
@@ -0,0 +1,10 @@
+#SELECT * FROM bank_v2.pracownicy;
+#SELECT * FROM bank_v2.stanowiska;
+#SELECT * FROM bank_v2.dzialy;
+#SELECT imie, nazwisko, pensja FROM bank_v2.pracownicy;
+#SELECT imie, nazwisko, pensja, dodatki  FROM bank_v2.pracownicy;
+#SELECT imie, nazwisko, pensja + dodatki as "wynagrodzenie calkowite" FROM bank_v2.pracownicy;
+#SELECT imie, nazwisko, pensja/30 as "Dniówka", pensja/30*6 as "tygodniówka", pensja as "miesięczna", pensja *12 "roczna" FROM bank_v2.pracownicy order by pensja DESC;
+#SELECT imie, nazwisko FROM bank_v2.pracownicy where nazwisko like "M%";
+#SELECT imie, nazwisko FROM bank_v2.pracownicy,bank_v2.dzialy where bank_v2.dzialy.nazwa = "logistyka" or bank_v2.dzialy.nazwa = "informatyka";
+SELECT imie, nazwisko FROM bank_v2.pracownicy;
