868cbda4bbe34cbcb49e509275ebf0f770323c15
Wojt <w.g.michalik@gmail.com>
2017-09-12 20:59:28 +0200

    10 zrobione

--- bank zapytania.sql
+++ bank zapytania.sql
@@ -7,4 +7,4 @@
 #SELECT imie, nazwisko, pensja/30 as "Dniówka", pensja/30*6 as "tygodniówka", pensja as "miesięczna", pensja *12 "roczna" FROM bank_v2.pracownicy order by pensja DESC;
 #SELECT imie, nazwisko FROM bank_v2.pracownicy where nazwisko like "M%";
 #SELECT imie, nazwisko FROM bank_v2.pracownicy,bank_v2.dzialy where bank_v2.dzialy.nazwa = "logistyka" or bank_v2.dzialy.nazwa = "informatyka";
-SELECT imie, nazwisko FROM bank_v2.pracownicy;
+#SELECT CONCAT_WS(' ', imie, nazwisko) AS "Imie Nazwisko", (Select CONCAT_WS(' ', imie, nazwisko)FROM bank_v2.pracownicy where id_pracownika = 150) as "przelozony" FROM bank_v2.pracownicy where id_przelozonego = 150;
